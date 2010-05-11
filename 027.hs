5369f6924261a28181bae284778907205e79e4d6
Lyndon Maydwell <maydwell@gmail.com>
2010-05-12 02:06:17 +0800

    Solved

--- 027.hs
+++ 027.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE BangPatterns #-}
 
+import Prime (isPrime)
 import Primes
 import Data.List (maximumBy)
 
@@ -10,7 +11,10 @@ prop_formula_a = 40 == nPrimes euler
   where euler = map (formula 1 41) [0..]
 
 prop_formula_b = 80 == nPrimes computer
-  where computer = map (formula 79 1601) [0..]
+  where computer = map (formula (-79) 1601) [0..]
+
+prop_formula_c = and (map isPrime computer)
+  where computer = map (formula (-79) 1601) [0..79]
 
 primeMakers :: [(Integer,Integer,Integer)]
 primeMakers = do
@@ -22,21 +26,14 @@ primeMakers = do
     r :: [Integer]
     r =  [-999 .. 999]
 
-nPrimes :: Integral a => [a] -> Integer
-nPrimes = nPrimes' primes
-
--- Bang to avoid a stack overflow.
-nPrimes' :: (Eq a, Ord a) => [a] -> [a] -> Integer
-nPrimes' [] _ = 0
-nPrimes' _ [] = 0
-nPrimes' (h:t) ! o@(x:xs)
-  | h <  x    = nPrimes' t o
-  | h == x    = 1 + nPrimes' t xs
-  | otherwise = 0
+nPrimes :: [Integer] -> Integer
+nPrimes = fromIntegral . length . takeWhile (isPrime)
 
 answer :: (Integer,Integer,Integer)
 answer = maximumBy f primeMakers
   where f (_,_,n1) (_,_,n2) = n1 `compare` n2
 
 main :: IO ()
-main =  print answer
+main = do
+  (x,y,_) <- return answer
+  print $ x * y
--- Primes.hs
+++ Primes.hs
@@ -1,6 +1,6 @@
 module Primes (
   module NumberTheory.Sieve.ONeill
-
 ) where
 
 import NumberTheory.Sieve.ONeill (primes)
+import NumberTheory.Sieve.Phi (isPrime)
