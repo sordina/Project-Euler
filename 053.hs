f772667a794768e8d23da960ddec3871f0cc7a84
Lyndon Maydwell <maydwell@gmail.com>
2012-07-04 18:34:52 +0800

    Faster

--- 053.hs
+++ 053.hs
@@ -3,8 +3,7 @@ main = print answer
   where
     answer    = length valid
     valid     = filter (> 1e6) potential
-    potential = [comb n r | n <- range, r <- range]
-    range     = [1..100]
+    potential = [comb n r | n <- [1..100], r <- [1..n]]
 
 comb :: Rational -> Rational -> Rational
 comb n r = (fact n) / (fact r * fact (n - r))
