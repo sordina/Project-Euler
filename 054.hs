62556984795c6fdc1f9ab89147d762c629a6bb6e
Lyndon Maydwell <lyndon@sordina.net>
2013-04-14 01:28:57 +0800

    Still not quite right...

--- 054.hs
+++ 054.hs
@@ -1,112 +1,24 @@
 import Control.Applicative
 import Control.Arrow
 import Control.Monad.State
+import Control.Monad.Maybe
+import Control.Monad.Identity
 import Data.List
-import Data.Function
+import Data.Maybe
 import Data.Ord
+import Data.Function
 import Text.Groom
 
-getRows :: IO [String]
-getRows = lines <$> readFile "Problems/poker.txt"
-
-main :: IO ()
-main = getRows >>= print . length . filter winner
-
-winner :: String -> Bool
-winner =   words
-       >>> (hand . take 5 &&& hand . drop 5)
-       >>> uncurry (>)
-
-hand :: [String] -> ClassifiedHand
-hand = (classify &&& reverse . sort) . map readCard
-
-readRank :: String -> Rank
-readRank = Rank . p
-  where
-    p :: String -> Int
-    p "T" = 10
-    p "J" = 11
-    p "Q" = 12
-    p "K" = 13
-    p "A" = 14
-    p x   = read x
-
-readCard :: String -> Card
-readCard [r,s] = Card (readRank [r]) (read [s])
-readCard c     = error $ "Invalid Card " ++ c
-
-classifications :: [StateT (MaybeT ClassifiedHand) ]
-classifications [ do suit <- sameSuit
-                     card <- has 14
-                     top  <- run
-                     return RoyalFlush
-
-                , do suit <- sameSuit
-                     top  <- run
-                     return $ StraightFlush $ rank top
-
-                , do top <- kind 4
-                     return $ FourOfAKind $ rank top
-
-                , do top1 <- kind 3
-                     top2 <- kind 2
-                     return $ FullHouse (rank top1) (rank top2)
-
-                , do top <- sameSuit
-                     return $ Flush $ rank top
-
-                , do top <- run
-                     return $ Straight $ rank top
-
-                , do top <- kind 3
-                     return $ ThreeOfAKind $ rank top
-
-                , do top1 <- kind 2
-                     top2 <- kind 2
-                     return $ TwoPairs (rank top1) (rank top2)
-
-                , do top1 <- kind 2
-                     return $ OnePair $ rank top1
-                ]
-
-highest = undefined
-
-classify :: Hand -> (Classification, Hand)
-classify h = fromMaybe (highest h) (find isJust ran)
-  where
-    ran = map (\x -> runState x h) classifications
-
-execConstraints :: Hand -> HandStateM -> Bool
-execConstraints h s = snd $ runState s (h, True)
-
-next :: (Hand -> HandState) -> HandStateM
-next f = do (s,b) <- get
-            when b $ put $ f s
-
-sameSuit :: HandStateM
-sameSuit = next $ id &&& samaSama . map suit
-
-has :: Int -> HandStateM
-has r = next $ id &&& any (== r) . map (unRank . rank)
-
-run :: HandStateM
-run = next $ id &&& (map (unRank . rank) >>> sort >>> zipWith (+) [1,0..] >>> samaSama)
-
-kind :: Int -> HandStateM
-kind n = next $ concat . filter ((/= n) . length) . grouper
-              &&&        any    ((== n) . length) . grouper
-  where
-    grouper = groupBy ((==) `on` rank) . sortBy (comparing rank)
-
-samaSama :: Eq x => [x] -> Bool
-samaSama l = and $ zipWith (==) l (tail l)
+-- | TODO: Use read instances instead for better succinctness -
+-- | http://stackoverflow.com/questions/5520940/creating-instance-of-read-type-class-in-haskell-for-custom-data-type
 
 -- Data
 
 type Hand           = [Card]
-type HandState      = (Hand, Bool)
-type HandStateM     = State HandState Classification
-type ClassifiedHand = (Classification, Hand)
+type CardA a        = StateT [Card] (MaybeT Identity) a
+type CardU          = CardA  ()
+type CardC          = CardA  Card
+type CardF          = CardA  Classification
 
 data Card = Card { rank :: Rank, suit :: Suit } deriving (Show, Eq, Ord)
 data Suit = S | C | D | H                       deriving (Show, Eq, Ord, Read)
@@ -124,13 +36,114 @@ data Classification = HighCard      Rank       --  Highest value card.
                     | RoyalFlush               --  Ten, Jack, Queen, King, Ace, in same suit.
                     deriving (Eq, Ord, Show)
 
--- Tests
+-- Functions
 
-test :: Bool -> String -> IO ()
-test b s = if b == winner s then print $ "Test Passed"
-                            else do
-                              print $ "Test Failed: " ++ s
-                              putStrLn $ groom $ hands s
+main :: IO ()
+main = getRows >>= print . length . filter winner
+
+getRows :: IO [String]
+getRows = lines <$> readFile "Problems/poker.txt"
+
+winner :: String -> Bool
+winner =   words
+       >>> (hand . take 5 &&& hand . drop 5)
+       >>> uncurry (>)
+
+hand :: [String] -> (Classification, [Int])
+hand = (classify &&& reverse . sort . map getRank) . map readCard
+
+readCard :: String -> Card
+readCard [r,s] = Card (readRank [r]) (read [s])
+readCard c     = error $ "Invalid Card " ++ c
+
+readRank :: String -> Rank
+readRank = Rank . p
+  where
+    p :: String -> Int
+    p "T" = 10
+    p "J" = 11
+    p "Q" = 12
+    p "K" = 13
+    p "A" = 14
+    p x   = read x
+
+classify :: Hand -> Classification
+classify hnd = fromMaybe (highest hnd) (join $ find isJust evaluated)
+   where
+    evaluated  = map (runIdentity . runMaybeT . flip evalStateT hnd) classifications
+
+classifications :: [ CardF ]
+classifications = [ do sameSuit
+                       has 14
+                       _ <- run
+                       return RoyalFlush
+
+                  , do sameSuit
+                       top  <- run
+                       return $ StraightFlush $ rank top
+
+                  , do top <- kind 4
+                       return $ FourOfAKind $ rank top
+
+                  , do top1 <- kind 3
+                       top2 <- kind 2
+                       return $ FullHouse (rank top1) (rank top2)
+
+                  , do sameSuit
+                       top <- best
+                       return $ Flush $ rank top
+
+                  , do top <- run
+                       return $ Straight $ rank top
+
+                  , do top <- kind 3
+                       return $ ThreeOfAKind $ rank top
+
+                  , do top1 <- kind 2
+                       top2 <- kind 2
+                       return $ TwoPairs (rank top1) (rank top2)
+
+                  , do top1 <- kind 2
+                       return $ OnePair $ rank top1
+                  ]
+
+highest :: Hand -> Classification
+highest = HighCard . rank . maximum
+
+best :: CardC
+best = maybeFail . listToMaybe . reverse . sort =<< get
+
+sameSuit :: CardU
+sameSuit = get >>= samaSama . map suit
+
+has :: Int -> CardU
+has r = get >>= guard . isJust . find ((==r) . getRank)
+
+getRank :: Card -> Int
+getRank = unRank . rank
+
+run :: CardC
+run = do cards <- gets $ map getRank >>> sort >>> zipWith (+) [1,0..]
+         samaSama cards
+         best
+
+kind :: Int -> CardC
+kind n = do cards    <- gets $ groupBy ((==) `on` rank) . sortBy (comparing rank)
+            matched  <- maybeFail $ find ((==n) . length) cards
+            put       $ concat . filter ((/= n) . length) $ cards
+            maybeFail $ listToMaybe $ matched
+
+samaSama :: Eq x => [x] -> CardU
+samaSama = guard . allSame
+
+allSame :: Eq x => [x] -> Bool
+allSame l = and $ zipWith (==) l (tail l)
+
+maybeFail :: Maybe a -> CardA a
+maybeFail (Just a) = return a
+maybeFail Nothing  = mzero
+
+-- Tests
 
 tests :: IO ()
 tests = mapM_ (uncurry test) [(False, "5H 5C 6S 7S KD 2C 3S 8S 8D TD")
@@ -139,5 +152,12 @@ tests = mapM_ (uncurry test) [(False, "5H 5C 6S 7S KD 2C 3S 8S 8D TD")
                              ,(True,  "4D 6S 9H QH QC 3D 6D 7H QD QS")
                              ,(True,  "2H 2D 4C 4D 4S 3C 3D 3S 9S 9D")]
 
-hands :: String -> (ClassifiedHand, ClassifiedHand)
-hands = words >>> (hand . take 5 &&& hand . drop 5)
+  where
+
+    test :: Bool -> String -> IO ()
+    test b s = if b == winner s then print $ "Test Passed"
+                                else do
+                                  print $ "Test Failed: " ++ s
+                                  putStrLn $ groom $ hands s
+
+    hands = words >>> (hand . take 5 &&& hand . drop 5)
